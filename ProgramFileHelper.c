13cc587f0a5ce15933af955de7f3744a4649426e
SamarthB2000 <149254311+SamarthB2000@users.noreply.github.com>
2023-10-28 20:38:22 +0530

    Add files via upload

---
+++ ProgramFileHelper.c
@@ -0,0 +1,17 @@
+#include"ProgramFileHeader.h"
+
+///////////////////////////////////////////////////////////////////////////////////
+// Function Name : Addition
+// Input         : Unsigned integer , Unsigned integer
+// Output        : Unsigned integer
+// Description   : Performs Addition of two number 
+// Author        : Pratik Dinkar Jawanjal
+// Date          : 18/04/2023 + 19/04/2023
+///////////////////////////////////////////////////////////////////////////////////
+
+unsigned int Addition(unsigned int iValue1 , unsigned int iValue2)
+{
+    unsigned int iRet = 0;
+    iRet = iValue1 + iValue2;
+    return iRet;
+}
